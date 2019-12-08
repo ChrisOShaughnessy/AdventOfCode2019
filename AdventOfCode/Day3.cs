94f040fd3e43f02a6cf5093f91f171a497740eb6
choshag24 O'Shaughnessy <c.oshaughnessy15@gmail.com>
2019-12-08 17:23:41 -0500

    Part One of Day three

---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day3
+    {
+        public string WireOneInput { get; set; }
+        public string WireTwoInput { get; set; }
+
+        public Day3()
+        {
+            int counter = 0;
+            var file = new  System.IO.StreamReader(@".\Data\DayThree.txt");
+            string line;
+            while((line = file.ReadLine()) != null)
+            {
+                if(counter == 0)
+                {
+                    WireOneInput = line;
+                }
+                else
+                {
+                    WireTwoInput = line;
+                }
+                counter++;
+            }
+        }
+
+        public int ClosestManhattanDistanceFromCenter()
+        {
+            var pathOne = CalculatePathOfWire(WireOneInput);
+            var pathTwo = CalculatePathOfWire(WireTwoInput);
+            return pathOne.Intersect(pathTwo).Select(p => Math.Abs(p.x) + Math.Abs(p.y)).Min(); 
+        }
+
+        private List<(int x, int y)> CalculatePathOfWire(string input)
+        {
+            var path = new List<(int x, int y)>();
+            var inputs = input.Split(',');
+            foreach(var move in inputs)
+            {
+                var direction = move[0];
+                var distance = int.Parse(move.Substring(1));
+                var previousX = path.Count == 0 ? 0 : path[path.Count - 1].x;
+                var previousY = path.Count == 0 ? 0 : path[path.Count - 1].y;
+                var counter = 1;
+                if (direction == 'U')
+                {
+                    while(counter <= distance)
+                    {
+                        path.Add((previousX, previousY + counter));
+                        counter++;
+                    }
+                }
+                else if (direction == 'D')
+                {
+                    while (counter <= distance)
+                    {
+                        path.Add((previousX, previousY - counter));
+                        counter++;
+                    }
+                }
+                else if (direction == 'R')
+                {
+                    while (counter <= distance)
+                    {
+                        path.Add((previousX + counter, previousY));
+                        counter++;
+                    }
+                }
+                else if (direction == 'L')
+                {
+                    while (counter <= distance)
+                    {
+                        path.Add((previousX - counter, previousY));
+                        counter++;
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid Direction");
+                }
+                
+            }
+            return path;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 
-namespace AdventOfCode {
-  class Program {
-    static void Main(string[] args) {
-      //var rocketEquation = new RocketEquation();
-      //Console.WriteLine(rocketEquation.GetTotalFuelRequirements());
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var rocketEquation = new RocketEquation();
+            //Console.WriteLine(rocketEquation.GetTotalFuelRequirements());
 
-      var intCodeComputer = new IntCodeComputer();
-      //intCodeComputer.Parse();
-      var result = intCodeComputer.DetermineStartingInput(19690720);
-      if (result != null) {
-        Console.WriteLine(result.Item1);
-        Console.WriteLine(result.Item2);
-      }
+            //var intCodeComputer = new IntCodeComputer();
+            ////intCodeComputer.Parse();
+            //var result = intCodeComputer.DetermineStartingInput(19690720);
+            //if (result != null) {
+            //  Console.WriteLine(result.Item1);
+            //  Console.WriteLine(result.Item2);
+            //}
+
+            var DayThree = new Day3();
+            Console.WriteLine(DayThree.ClosestManhattanDistanceFromCenter());
+        }
     }
-  }
 }
